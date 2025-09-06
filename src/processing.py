f73dc123d54d9cf1900541b1f48ab58ce5690c4c
Dmitry Dudarev <dudarev-d-a@yandex.com>
2025-09-07 05:12:16 +1000

    minor fixes

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,7 @@ def filter_by_state(operations: list[dict], desired_state: str = "EXECUTED") -> 
     принимает список словарей и опционально значение для ключа state и возвращает новый список словарей,
     содержащий только те словари, у которых ключ state соответствует указанному значению
     """
-    new_operations_list = [i for i in operations if i['state'] == desired_state]
+    new_operations_list = [i for i in operations if i["state"] == desired_state]
 
     return new_operations_list
 
@@ -13,6 +13,6 @@ def sort_by_date(operations: list[dict], order_of_sorting: bool = True) -> list[
     принимает список словарей и необязательный параметр, задающий порядок сортировки
     и возвращает новый список, отсортированный по дате
     """
-    new_operations_list = sorted(operations, key=lambda x: x['date'], reverse=order_of_sorting)
+    new_operations_list = sorted(operations, key=lambda x: x["date"], reverse=order_of_sorting)
 
     return new_operations_list
