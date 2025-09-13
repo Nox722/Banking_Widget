cddfcb5c57e459d28f64f1af61c0f621b772f1d1
Dmitry Dudarev <dudarev-d-a@yandex.com>
2025-09-14 01:47:17 +1000

    добавлены тесты к модулю widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,38 @@
+from datetime import datetime
+
 from .masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_or_account_info: str) -> str:
+def mask_account_card(card_or_account_info: str | int) -> str:
     """принимает на вход тип и номер карты или счета и возвращает его маску"""
-    card_or_account = "".join([i for i in card_or_account_info if i.isalpha()])
+    card_or_account_info_str = str(card_or_account_info)
 
-    list_of_digits = [i for i in card_or_account_info if i.isdigit()]
-    number = int("".join(list_of_digits))
+    card_or_account = "".join([i for i in card_or_account_info_str if i.isalpha()])
 
-    if len(str(number)) > 16:
+    list_of_digits = [i for i in card_or_account_info_str if i.isdigit()]
+    digits_str = "".join(list_of_digits)
+
+    if not digits_str:
+        return "Ошибка. Пожалуйста, введите номер карты или счёта."
+
+    number = int(digits_str)
+
+    if len(str(number)) == 20 and card_or_account.lower() in ["счет", "счёт", "account", ""]:
+        if card_or_account.lower() == "":
+            return get_mask_account(number)
         return card_or_account + " " + get_mask_account(number)
+    elif len(str(number)) == 16 and card_or_account.lower() in ["карта", "visa", "mastercard", "card", ""]:
+        if card_or_account.lower() == "":
+            return get_mask_card_number(number)
+        return card_or_account + " " + get_mask_card_number(number)
+    else:
+        return "Ошибка. Проверьте правильность введённых данных."
 
-    return card_or_account + " " + get_mask_card_number(number)
 
-
-def get_date(date: str) -> str:
+def get_date(date_str: str) -> str:
     """принимает на вход дату в формате YYYY-MM-DDThh:mm:ss.hhmmss и возвращает строку с датой в формате DD.MM.YYYY"""
-    formatted_date = ".".join(date[:10].split("-")[::-1])
-    return formatted_date
+    try:
+        dt = datetime.fromisoformat(date_str)
+        return dt.strftime("%d.%m.%Y")
+    except ValueError:
+        return "Ошибка. Некорректный формат даты."
