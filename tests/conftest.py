fc2efbd6a3f07316279b906f9334cfed1607945d
Dmitry Dudarev <dudarev-d-a@yandex.com>
2025-09-14 19:18:06 +1000

    добавлены тесты к модулю processing.py и файл conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,79 @@
+from typing import Any
+
+import pytest
+
+
+@pytest.fixture
+def list_of_dicts() -> list[dict[str, Any]]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def list_of_dicts_state_executed() -> list[dict[str, Any]]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+@pytest.fixture
+def list_of_dicts_state_canceled() -> list[dict[str, Any]]:
+    return [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def list_of_dicts_sorting_by_descending_date() -> list[dict[str, Any]]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+@pytest.fixture
+def list_of_dicts_sorting_by_increasing_date() -> list[dict[str, Any]]:
+    return [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
+
+@pytest.fixture
+def list_of_dicts_without_date() -> list[dict[str, Any]]:
+    return [
+        {"id": 41428829, "state": "EXECUTED"},
+        {"id": 939719570, "state": "EXECUTED"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def list_of_dicts_without_date_sorted() -> list[dict[str, Any]]:
+    return [
+        {"id": 41428829, "state": "EXECUTED"},
+        {"id": 939719570, "state": "EXECUTED"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    ]
+
+
+@pytest.fixture
+def list_of_dicts_unexpected_date() -> list[dict[str, Any]]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "03/07/2019 18:35:29"},
+        {"id": 939719570, "state": "EXECUTED", "date": "30-06-2018T02:08:58"},
+        {"id": 594226727, "state": "CANCELED", "date": "12.09.2018 21:27:25"},
+        {"id": 615064591, "state": "CANCELED", "date": "14/10/2018 08:21:33"},
+    ]
